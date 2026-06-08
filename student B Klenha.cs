f1f40f3183dbe29892b7f116936d2ecc46059a23
richardklenha <166384777+richardklenha@users.noreply.github.com>
2026-06-08 18:52:32 +0200

    Add files via upload

---
+++ student B Klenha.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MojeEvidenceApp.Models
+{
+    public class Polozka
+    {
+        
+        public string Nazev { get; set; }
+        public string Zanr { get; set; }
+        public int Delka { get; set; } 
+        public int Hodnoceni { get; set; } 
+        public bool Precteno { get; set; } 
+
+        
+        public static bool JeVstupVPoradku(string nazev, string delkaText, out string chyba)
+        {
+            chyba = "";
+            
+            if (string.IsNullOrWhiteSpace(nazev))
+            {
+                chyba = "Musíš zadat název!";
+                return false; 
+            }
+            
+            if (!int.TryParse(delkaText, out int vyslednaDelka) || vyslednaDelka <= 0)
+            {
+                chyba = "Délka musí být číslo a větší než 0!";
+                return false;
+            }
+            
+            return true; 
+        }
+    }
+}
